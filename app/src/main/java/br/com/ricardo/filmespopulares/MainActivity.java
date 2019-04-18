ca74942d2a17d35720e70fd854e5fa8b361ca753
rickale <ti.ricardo.sousa@gmail.com>
2019-04-18 03:47:10 -0300

    Criado a estrutura principal de telas. Com os 2 Fragments para lista de filmes populares e de favoritos e o TabLayout.

--- app/src/main/java/br/com/ricardo/filmespopulares/MainActivity.java
+++ app/src/main/java/br/com/ricardo/filmespopulares/MainActivity.java
@@ -1,13 +1,71 @@
 package br.com.ricardo.filmespopulares;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbarMain;
+    private TabLayout tabLayoutMain;
+    private ViewPager viewPagerMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbarMain = (Toolbar) findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbarMain);
+
+        viewPagerMain = (ViewPager) findViewById(R.id.viewpager_main);
+        setupViewPager(viewPagerMain);
+
+        tabLayoutMain = (TabLayout) findViewById(R.id.tabs_main);
+        tabLayoutMain.setupWithViewPager(viewPagerMain);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new PopularMoviesFragment(), "POPULARES");
+        adapter.addFragment(new FavoriteMoviesFragment(), "FAVORITOS");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
