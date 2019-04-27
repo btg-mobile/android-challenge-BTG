931d639278b4ec765472754b1b14f65f839ef79d
gbl147852 <gabriel.linss@hotmail.com>
2019-04-27 20:22:14 -0300

    Fix Dagger + Gradle

--- app/src/main/java/com/example/desafiobtg/di/ActivityBindModule.kt
+++ app/src/main/java/com/example/desafiobtg/di/ActivityBindModule.kt
@@ -1,8 +1,16 @@
 package com.example.desafiobtg.di
 
+import com.example.desafiobtg.di.qualifiers.ActivityScoped
+import com.example.desafiobtg.usecases.MainActivity
+import com.example.desafiobtg.usecases.MainModule
 import dagger.Module
+import dagger.android.ContributesAndroidInjector
 
 @Module
 abstract class ActivityBindModule {
+
+    @ActivityScoped
+    @ContributesAndroidInjector(modules = [MainModule::class])
+    internal abstract fun mainActivity(): MainActivity
 
 }
