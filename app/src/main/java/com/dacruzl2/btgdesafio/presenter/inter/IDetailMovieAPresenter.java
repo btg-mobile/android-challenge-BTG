cf423a5cc1bd51eb261a3d5ab1c4421528bccfce
dacruzl2 <leonardo_cruz09@hotmail.com>
2019-04-29 13:19:02 -0300

    teste

--- app/src/main/java/com/dacruzl2/btgdesafio/presenter/inter/IDetailMovieAPresenter.java
+++ app/src/main/java/com/dacruzl2/btgdesafio/presenter/inter/IDetailMovieAPresenter.java
@@ -3,6 +3,6 @@ package com.dacruzl2.btgdesafio.presenter.inter;
 import android.content.Context;
 
 public interface IDetailMovieAPresenter {
-   // void retrieveGenre(Context context);
+   //  void retrieveGenre(Context context);
 
 }
