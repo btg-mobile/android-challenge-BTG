a0a12bb39b4a71514d8ef32ef2a6db8ce2f4b4e5
Artur Kida <ackida@gmail.com>
2019-08-11 15:30:54 -0300

    adding Mockito dependencies

---
+++ app/src/test/java/com/arturkida/popularmovies_kotlin/ui/home/MoviesViewModelTest.kt
@@ -0,0 +1,52 @@
+package com.arturkida.popularmovies_kotlin.ui.home
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MoviesViewModelTest {
+
+    @Test
+    fun getGenres() {
+    }
+
+    @Test
+    fun getPopularMovies() {
+    }
+
+    @Test
+    fun getFilteredMovies() {
+    }
+
+    @Test
+    fun setFilteredMovies() {
+    }
+
+    @Test
+    fun getFavoriteMovies() {
+    }
+
+    @Test
+    fun mustShowMoviesList() {
+    }
+
+    @Test
+    fun populateGenresNameFrom() {
+    }
+
+    @Test
+    fun updateFavoriteStatusOf() {
+    }
+
+    @Test
+    fun getPopularMovies1() {
+    }
+
+    @Test
+    fun getGenres1() {
+    }
+
+    @Test
+    fun searchMovies() {
+    }
+}
