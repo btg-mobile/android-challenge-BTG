20cb1297219bbb7e89fb269d4c43e18db01297a3
raphael rocha <raphaelrocha86@gmail.com>
2019-08-13 19:10:33 -0400

    primeiro layout da tela de login.

--- app/src/main/java/com/androidchallengebtg/MainActivity.java
+++ app/src/main/java/com/androidchallengebtg/MainActivity.java
@@ -3,6 +3,12 @@ package com.androidchallengebtg;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/*
+user: btgchallenge
+email: raphaelrocha86+btg@gmail.com
+pass: 2405
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
